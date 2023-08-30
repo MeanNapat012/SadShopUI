8cb902e0794a01142b96ab1bd47947eb6bd43bdd
MRPAO <aungpaosk139@gmail.com>
2023-08-30 21:38:09 +0700

    ADD: UIcodeV.1

---
+++ Assets/Script/UICode.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InventorySystem{
+    public class UICode : MonoBehaviour
+    {
+        /*
+        [Header("Category")]
+        [SerializeField] Image categoryIcon;
+        [SerializaField] Text categoryText;
+
+        [Header("Curent Item")]
+        [SerializeField] Image currentItemIcon;
+        [SerializeField] Text description;
+
+        [Header("Item List")]
+        [SerializeField] UIItem itemPrefab;
+        [SerializeField] List<> = new ;
+        // Start is called before the first frame update
+        void Start()
+        {
+            itemUIPrefab.gameObject.setActive(false);
+        }
+
+        public void SetCategory(CategoryInfo info){
+            categoryIconImage.sprite = info.icon;
+            categoryText.text = info.name;
+        }
+
+        public void SetCurrentItemInfo(Itemdata data){
+            descriptionText.text = data.description;
+            currentItemIconImage.sprite = data.icon;
+        }
+
+        public void SetItem(UIItem_data[] uiData){
+
+        }
+        public void ClearAllItemUI(){
+
+        }
+        [Serializable]
+        public class CategoryInfo{
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        */
+    }
+}
