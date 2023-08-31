1005343818bee03eaa88b859d9724a6717504448
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2023-08-31 18:45:53 +0700

    ADD: Item data FEAT: Purchase System Template

---
+++ Assets/Scripts/ShopStore.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Toem.ShopSystem
+{
+    public class ShopStore : MonoBehaviour
+    {
+        public ItemData[] ItemArray => itemList.ToArray();
+        [SerializeField] List<ItemData> itemList = new List<ItemData>();
+
+        public ItemData[] GetItemsByType(ItemType targetType)
+        {
+            var resultList = new List<ItemData>();
+            foreach(var itemData in itemList)
+            {
+                if(itemData.Type == targetType) resultList.Add(itemData);
+            }
+            return resultList.ToArray();
+        }
+
+        public void Purchase()
+        {
+
+        }
+    }
+
+    [Serializable]
+    public class ItemData
+    {
+        public string itemName;
+        public string itemdascription;
+        public Sprite itemShop;
+        public ItemType Type;
+        public int purchase;
+    }
+
+    public enum ItemType
+    {
+        one,
+        two,
+        three,
+        Others
+    }
+}
